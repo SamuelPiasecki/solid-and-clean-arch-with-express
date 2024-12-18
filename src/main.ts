e6e0a2ed6d7b816edf21b7bdf879c10753bfab6a
Samuel Piasecki <samupiasecki@gmail.com>
2024-12-18 15:56:28 -0300

    Basic API created with express

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { ApiExpress } from "./infra/api/express/api.express";
+import { CreateProductRoute } from "./infra/api/express/routes/product/create-product.express.route";
+import { ListProductRoute } from "./infra/api/express/routes/product/list-product.express.route";
+import { ProductRepositoryPrisma } from "./infra/repositories/product/product.repository.prisma";
+import { prisma } from "./package/prisma/prisma";
+import { CreateProductUseCase } from "./usecases/create-product/create-product.usecase";
+import { ListProductUsecase } from "./usecases/list-product/list-product.usecase";
+
+function main() {
+  const aRepository = ProductRepositoryPrisma.create(prisma);
+
+  const createProductUseCase = CreateProductUseCase.create(aRepository);
+  const listProductUsecase = ListProductUsecase.create(aRepository);
+
+  const createRoute = CreateProductRoute.create(createProductUseCase);
+  const listRoute = ListProductRoute.create(listProductUsecase);
+
+  const api = ApiExpress.create([createRoute, listRoute]);
+  const port = 8000;
+  api.start(port);
+}
+
+main();
